d662b29fcc8e1a3fa0ecfb606c356e5b7c31671d
Renata Detros <renata.detros@gmail.com>
2023-03-16 12:41:33 -0300

    exercicio de fixacao realizados 1 e 2

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,35 @@
 //CODIGO AQUI
+//2x2+2
+let a = 2
+console.log(a*a+2);
+
+//2x(2+2)
+let b = 2*(2+2)
+console.log(b)
+
+
+console.log (5===5);
+console.log(5!=="5");
+console.log("5"==="cinco");
+console.log(typeof 5===typeof 20);
+console.log(typeof 5===typeof "cinco");
+
+
+//crie duas variaveis
+let x = 40
+let y = 100
+// utilizando apenas os >   <  >= <=  faça as comparações
+
+// o primeiro numero é igual ao segundo? True / False
+console.log (x === y)
+
+// o primeiro numero é menor ou igual ao segundo? True / False
+console.log (x <= y)
+
+// o primeiro numero é maior que o segundo? True / False
+console.log (x > y)
+
+// o primeiro numero é menor que o segundo? True / False
+console.log (x > y)
+
+
--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,2 +1,19 @@
 // EXERCICIO DE FIXAÇÃO
+//Faça um programa que pergunte:
+//sua idade usando prompt
+//pergunte a idade do seu melhor amigo 
+//imprima no console sua idade é maior que a do seu melhor amigo? verdadeiro ou falso
+//imprima a diferença de idade
+
+
+let minhaIdade = Number(prompt("Qual é a sua idade?"));
+let idadeAmigo = Number(prompt("Qual é a idade do seu melhor amigo?"));
+
+let diferencaIdade = (minhaIdade - idadeAmigo);
+
+let minhaIdadeMaior = minhaIdade > idadeAmigo; // retorna true se a minha idade for maior que a do meu amigo
+
+console.log("Sua idade é maior que a do seu melhor amigo?", minhaIdadeMaior);
+console.log("A diferença de idade entre vocês é de", diferencaIdade, "anos.");
+
 
